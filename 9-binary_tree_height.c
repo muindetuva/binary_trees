571abdd7031798e82996819acd024efb5a86df9c
musangisilvia <musangisilvia@gmail.com>
2021-07-22 06:15:30 +0000

    correct gcc error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
 	size_t height_l;
 	size_t height_r;
+
+	if (tree == NULL)
+		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
