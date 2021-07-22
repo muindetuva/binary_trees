806841c7b45e2353d773d3ec0ab2d539d38135c0
musangisilvia <musangisilvia@gmail.com>
2021-07-22 06:09:24 +0000

    fix betty errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
   * binary_tree_delete - Deletes an entire binary tree
-  * @tree - A pointer to the root node of the tree to delete
+  * @tree: A pointer to the root node of the tree to delete
   */
 
 void binary_tree_delete(binary_tree_t *tree)
