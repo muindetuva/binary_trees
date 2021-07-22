515f733707d6d336df9e5d03f3becf11415f169a
musangisilvia <musangisilvia@gmail.com>
2021-07-22 06:03:08 +0000

    fix betty errors

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,7 +2,7 @@
 /**
   * binary_tree_inorder - Goes through a binary tree using inorder traversal
   * @tree: A pointer to the root node of the tree to traverse
-  * @func: A pointer to a function to call for each node. 
+  * @func: A pointer to a function to call for each node.
   */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
