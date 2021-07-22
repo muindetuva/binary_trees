ac53b49050e3b850128c97ab86d1682a48f40f56
musangisilvia <musangisilvia@gmail.com>
2021-07-22 06:02:30 +0000

    fix betty errors

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_postorder - Goes through a binary tree using postorder traversal
+  * binary_tree_postorder - traverses a binary tree using postorder
   * @tree: A pointer to the root node of the tree to traverse
-  * @func: A pointer to a function to call for each node. 
+  * @func: A pointer to a function to call for each node.
   */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
